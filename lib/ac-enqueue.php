ea02c284d747a8ebd5737fa70f3e9adc4f6c5d8c
Richiearnold <richard@ambercouch.co.uk>
2013-10-26 17:56:29 +0100

    Organise. assets folder for js and sass/css lib folder for php, template folder for content parts. functions.php just to calls the custom pho file ac-inuk which in turn calls all the different function

---
+++ lib/ac-enqueue.php
@@ -0,0 +1,21 @@
+
+<?php
+/**
+ * Enqueue scripts and styles
+ */
+function ac_inuk_scripts() {
+	wp_enqueue_style( 'ac inuk-style', get_stylesheet_uri() );
+
+	wp_enqueue_script( 'ac inuk-navigation', get_template_directory_uri() . 'assets/js/navigation.js', array(), '20120206', true );
+
+	wp_enqueue_script( 'ac inuk-skip-link-focus-fix', get_template_directory_uri() . 'assets/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+
+	if ( is_singular() && wp_attachment_is_image() ) {
+		wp_enqueue_script( 'ac inuk-keyboard-image-navigation', get_template_directory_uri() . 'assets/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'ac_inuk_scripts' );
