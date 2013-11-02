be509d444b286c2fe54de8eb7dc87396900ef0a8
Richiearnold <richard@ambercouch.co.uk>
2013-11-02 17:50:17 +0000

    Added body data to header.php. Created ac-body-data.php function. Created and enqueued ac-inuk.js

--- lib/ac-enqueue.php
+++ lib/ac-enqueue.php
@@ -5,17 +5,19 @@
  */
 function ac_inuk_scripts() {
 	wp_enqueue_style( 'ac_inuk-style', get_stylesheet_uri() );
+        
+        wp_enqueue_script( 'ac_inuk', get_template_directory_uri() . '/assets/js/ac-inuk.js', array('jquery'), '20120206', true );
 
-	wp_enqueue_script( 'ac_inuk-navigation', get_template_directory_uri() . 'assets/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'ac_inuk-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'ac_inuk-skip-link-focus-fix', get_template_directory_uri() . 'assets/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'ac_inuk-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
 	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'ac_inuk-keyboard-image-navigation', get_template_directory_uri() . 'assets/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+		wp_enqueue_script( 'ac_inuk-keyboard-image-navigation', get_template_directory_uri() . '/assets/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'ac_inuk_scripts' );
--- lib/ac-inuk.php
+++ lib/ac-inuk.php
@@ -8,6 +8,7 @@ require( get_template_directory() . '/lib/extras/theme_setup.php' );
 require( get_template_directory() . '/lib/extras/custom-background.php' );
 require( get_template_directory() . '/lib/ac-widget-areas.php' );
 require( get_template_directory() . '/lib/ac-enqueue.php' );
+require( get_template_directory() . '/lib/ac-body-data.php' );
 
 /**
  * Implement the Custom Header feature
