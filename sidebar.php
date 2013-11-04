b4e9cf69bfa7668f5aa66fe0b95c9c644fcef67e
ambercouch <richard@ambercouch.co.uk>
2013-11-04 20:28:48 +0000

    Changes to the grid__item structure of the sidebar

--- sidebar.php
+++ sidebar.php
@@ -4,8 +4,9 @@
  *
  * @package ac inuk
  */
-?>
-	<div id="secondary" class="widget-area" role="complementary">
-		<?php do_action( 'before_sidebar' ); ?>
-		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?><?php endif; // end sidebar widget area ?>
-	</div><!-- #secondary -->
+?>      <div id="secondary" class="grid__item" role="complementary">
+            <divclass="widget-area" >
+                <?php do_action( 'before_sidebar' ); ?>
+                <?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?><?php endif; // end sidebar widget area ?>
+            </div>
+        </div><!-- #secondary -->
