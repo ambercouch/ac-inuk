52a0ca485fff201a7c0dfab323fc770a1cbf238c
Richiearnold <richard@ambercouch.co.uk>
2014-04-12 18:47:46 +0100

    Removed footer

--- footer.php
+++ footer.php
@@ -15,10 +15,7 @@
     <div class="grid">
       <div class="footer--master__site-info" >
         <div class="site-info">
-          <?php do_action('ac_inuk_credits'); ?>
-          <a href="http://wordpress.org/" title="<?php esc_attr_e('A Semantic Personal Publishing Platform', 'ac_inuk'); ?>" rel="generator"><?php printf(__('Proudly powered by %s', 'ac_inuk'), 'WordPress'); ?></a>
-          <span class="sep"> | </span>
-          <?php printf(__('Theme: %1$s by %2$s.', 'ac_inuk'), 'ac inuk', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>'); ?>
+          Footer Text
         </div><!-- .site-info -->
       </div>
     </div>
