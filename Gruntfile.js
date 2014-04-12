7b5fc16da7db65e6a7e9ad07193515b869a23280
Richiearnold <richard@ambercouch.co.uk>
2014-04-12 19:04:15 +0100

    added watch to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,6 @@ module.exports = function(grunt) {
     // 1. All configuration goes here
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
         watch: {
             css: {
               files: 'assets/scss/**/*.scss',
@@ -50,6 +49,7 @@ module.exports = function(grunt) {
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
