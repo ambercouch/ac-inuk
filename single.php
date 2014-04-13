86d503da9313b282bbe853debef68596d3ef76f7
Richiearnold <richard@ambercouch.co.uk>
2014-04-13 10:05:48 +0100

    Added grid to single.php

--- single.php
+++ single.php
@@ -4,30 +4,30 @@
  *
  * @package ac inuk
  */
-
-get_header(); ?>
+get_header();
+?>
 <div class="grid" >
-	<div id="primary" class="grid__item content-area">
-		<div id="content" class="site-content" role="main">
+  <div id="primary" class="content__single-post">
+    <div id="content" class="single-post" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+      <?php while (have_posts()) : the_post(); ?>
 
-			<?php get_template_part( 'templates/content', 'single' ); ?>
+        <?php get_template_part('templates/content', 'single'); ?>
 
-			<?php ac_inuk_content_nav( 'nav-below' ); ?>
+        <?php ac_inuk_content_nav('nav-below'); ?>
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' != get_comments_number() )
-					comments_template();
-			?>
+        <?php
+        // If comments are open or we have at least one comment, load up the comment template
+        if (comments_open() || '0' != get_comments_number())
+          comments_template();
+        ?>
 
-		<?php endwhile; // end of the loop. ?>
+      <?php endwhile; // end of the loop. ?>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+    </div><!-- #content -->
+  </div><!-- #primary -->
 
-        <?php get_sidebar(); ?>
+  <?php get_sidebar(); ?>
 
-        </div><!-- /.grid -->
+</div><!-- /.grid -->
 <?php get_footer(); ?>
