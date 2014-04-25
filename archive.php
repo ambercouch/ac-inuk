1c335bc6fb9bcec75c73a78866222267999ac4be
Richiearnold <richard@ambercouch.co.uk>
2014-04-25 22:21:04 +0100

    updated archive classes

--- archive.php
+++ archive.php
@@ -9,79 +9,81 @@
 get_header();
 ?>
 <div class="grid" >
-  <section id="primary" class="grid__item content-area">
-    <div id="content" class="site-content" role="main">
+  <section id="primary" class="content__post-archive">
+    <div id="content" class="post-archive" role="main">
 
       <?php if (have_posts()) : ?>
 
-        <header class="header">
-          <h1 class="header__title">
+        <header class="post__header">
+          <div class="header--article">
+            <h1 class="header--article__title">
+              <?php
+              if (is_category()) :
+                printf(__('Category Archives: %s', 'ac_inuk'), '<span class="title--article title--category" >' . single_cat_title('', false) . '</span>');
+
+              elseif (is_tag()) :
+                printf(__('Tag Archives: %s', 'ac_inuk'), '<span class="title--article title--tag" >' . single_tag_title('', false) . '</span>');
+
+              elseif (is_author()) :
+                /* Queue the first post, that way we know
+                 * what author we're dealing with (if that is the case).
+                 */
+                the_post();
+                printf(__('Author Archives: %s', 'ac_inuk'), '<span class="vcard"><a class="url fn n" href="' . esc_url(get_author_posts_url(get_the_author_meta('ID'))) . '" title="' . esc_attr(get_the_author()) . '" rel="me">' . get_the_author() . '</a></span>');
+                /* Since we called the_post() above, we need to
+                 * rewind the loop back to the beginning that way
+                 * we can run the loop properly, in full.
+                 */
+                rewind_posts();
+
+              elseif (is_day()) :
+                printf(__('Daily Archives: %s', 'ac_inuk'), '<span>' . get_the_date() . '</span>');
+
+              elseif (is_month()) :
+                printf(__('Monthly Archives: %s', 'ac_inuk'), '<span>' . get_the_date('F Y') . '</span>');
+
+              elseif (is_year()) :
+                printf(__('Yearly Archives: %s', 'ac_inuk'), '<span>' . get_the_date('Y') . '</span>');
+
+              elseif (is_tax('post_format', 'post-format-aside')) :
+                _e('Asides', 'ac_inuk');
+
+              elseif (is_tax('post_format', 'post-format-image')) :
+                _e('Images', 'ac_inuk');
+
+              elseif (is_tax('post_format', 'post-format-video')) :
+                _e('Videos', 'ac_inuk');
+
+              elseif (is_tax('post_format', 'post-format-quote')) :
+                _e('Quotes', 'ac_inuk');
+
+              elseif (is_tax('post_format', 'post-format-link')) :
+                _e('Links', 'ac_inuk');
+
+              else :
+                _e('Archives', 'ac_inuk');
+
+              endif;
+              ?>
+            </h1>
             <?php
             if (is_category()) :
-              printf(__('Category Archives: %s', 'ac_inuk'), '<span>' . single_cat_title('', false) . '</span>');
+              // show an optional category description
+              $category_description = category_description();
+              if (!empty($category_description)) :
+                echo apply_filters('category_archive_meta', '<div class="taxonomy-description">' . $category_description . '</div>');
+              endif;
 
             elseif (is_tag()) :
-              printf(__('Tag Archives: %s', 'ac_inuk'), '<span>' . single_tag_title('', false) . '</span>');
-
-            elseif (is_author()) :
-              /* Queue the first post, that way we know
-               * what author we're dealing with (if that is the case).
-               */
-              the_post();
-              printf(__('Author Archives: %s', 'ac_inuk'), '<span class="vcard"><a class="url fn n" href="' . esc_url(get_author_posts_url(get_the_author_meta('ID'))) . '" title="' . esc_attr(get_the_author()) . '" rel="me">' . get_the_author() . '</a></span>');
-              /* Since we called the_post() above, we need to
-               * rewind the loop back to the beginning that way
-               * we can run the loop properly, in full.
-               */
-              rewind_posts();
-
-            elseif (is_day()) :
-              printf(__('Daily Archives: %s', 'ac_inuk'), '<span>' . get_the_date() . '</span>');
-
-            elseif (is_month()) :
-              printf(__('Monthly Archives: %s', 'ac_inuk'), '<span>' . get_the_date('F Y') . '</span>');
-
-            elseif (is_year()) :
-              printf(__('Yearly Archives: %s', 'ac_inuk'), '<span>' . get_the_date('Y') . '</span>');
-
-            elseif (is_tax('post_format', 'post-format-aside')) :
-              _e('Asides', 'ac_inuk');
-
-            elseif (is_tax('post_format', 'post-format-image')) :
-              _e('Images', 'ac_inuk');
-
-            elseif (is_tax('post_format', 'post-format-video')) :
-              _e('Videos', 'ac_inuk');
-
-            elseif (is_tax('post_format', 'post-format-quote')) :
-              _e('Quotes', 'ac_inuk');
-
-            elseif (is_tax('post_format', 'post-format-link')) :
-              _e('Links', 'ac_inuk');
-
-            else :
-              _e('Archives', 'ac_inuk');
+              // show an optional tag description
+              $tag_description = tag_description();
+              if (!empty($tag_description)) :
+                echo apply_filters('tag_archive_meta', '<div class="taxonomy-description">' . $tag_description . '</div>');
+              endif;
 
             endif;
             ?>
-          </h1>
-          <?php
-          if (is_category()) :
-            // show an optional category description
-            $category_description = category_description();
-            if (!empty($category_description)) :
-              echo apply_filters('category_archive_meta', '<div class="taxonomy-description">' . $category_description . '</div>');
-            endif;
-
-          elseif (is_tag()) :
-            // show an optional tag description
-            $tag_description = tag_description();
-            if (!empty($tag_description)) :
-              echo apply_filters('tag_archive_meta', '<div class="taxonomy-description">' . $tag_description . '</div>');
-            endif;
-
-          endif;
-          ?>
+          </div>
         </header><!-- .page-header -->
 
         <?php /* Start the Loop */ ?>
