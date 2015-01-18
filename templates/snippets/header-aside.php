4e0e4e714fd0782a7dcf49d9797ee3fc0f86b02c
richiearnold75 <richard@ambercouch.co.uk>
2015-01-18 14:22:07 +0000

    Added files

---
+++ templates/snippets/header-aside.php
@@ -0,0 +1,7 @@
+<div id="header-widgets" class="header--master__widget-area sidebar" role="complementary">
+  <div class="widget-area" >
+    <?php do_action('before_header-aside'); ?>
+    <?php if (!dynamic_sidebar('header-aside')) : ?><?php endif; // end sidebar widget area ?>
+  </div>
+</div><!-- #secondary -->
+
